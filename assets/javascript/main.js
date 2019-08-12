72f8c9d41c69e9260971f20d49e53ed76b8dfa88
tr3vbr0w <trevbrow@live.com>
2019-08-12 13:46:03 -0600

    changed some bootstrap properties. need to add start stop function and fix add sport bug

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,5 +1,5 @@
 // Set Variables for query, beginning array to push user input to, and apiKey
-var outdoorSports = ["skiing", "mountain biking", "climbing", "hiking"]
+var outdoorSports = ["skiing", "mountain biking", "climbing", "hiking", "water skiing", "skateboarding", "trail running", "kayaking", "surfing"]
 var isPlaying = false;
 var addSport = $("#add-sport").val();
 //Function to loop through array and create buttons for each index within the array
@@ -14,7 +14,7 @@ function renderButtons(){
     }
 }
 renderButtons();
-//Click event handler to loop add the value of the search box to the array. render buttons called at the end to add button to the header
+//Click event handler to loop add the value of the search box to the array. render buttons called at the end to add button to the header. preventDefault is somehow screwing up the function to prevent it from loading gifs correctly
 $('#newB').on('click', function(event){
     event.preventDefault();
     var addSport = $('#add-sport').val().trim();
@@ -37,10 +37,13 @@ $('.sports-button').on('click', function(){
         for (var i = 0; i < r.data.length; i++){
             var eachGifLocation = r.data[i].images.fixed_height.url;
             var rating = r.data[i].rating;
+            var ratingDisp = $('<p>').text('Rated: '+ rating)
+            ratingDisp.addClass('text-muted')
             var addGif =$('<img>').attr("src", eachGifLocation);
+            addGif.addClass('h-100');
             var newDiv = $('<div>').append(addGif);
-            newDiv.append($('<p>').text('Rated: '+ rating));
-            newDiv.addClass('innerGif col-4 h-25 m-1');
+            newDiv.append(ratingDisp);
+            newDiv.addClass('innerGif col-3 m-1');
             $('.gif-dump').prepend(newDiv);
         }
     });
